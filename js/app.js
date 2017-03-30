842b5f0e80d37fe992ee50889736c7d20e4b4e0b
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-03-30 18:42:16 +0100

    News displaying

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
-(function(){
+function displayNews(data){
+	var newsBox = document.getElementsByClassName("content-box");
+	for(var i = 0; i < newsBox.length; i++){
+		console.log(newsBox[i]);
+	}
+	// var dataLength = data.length;
+	// var boxLength = newsBox.length;
+	// for (var i = 0; i < boxLength; i++) { 
+ //    	console.log(data.articles.url);
+	// }
+}
+
+
+(function () {
 	var news = document.getElementById("content");
+	var newsBox = document.getElementsByClassName("content-box");
 	var url = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=af43753d90494370bb8627019c2b0941";
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", url, true);
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState < 4){
-			news.innerHTML = "loading .....";
+			console.log("loading");
 		}
 
 		if(xhr.readyState === 4 && xhr.status === 200){
-			var response = xhr.responseText;
-			news.innerHTML = response;
+			var response = JSON.parse(xhr.responseText);
+			var articles = response.articles;
+			var count = articles.length;
+
+			var html = "";
+			for(i = 0; i < count; i++){
+				var url = articles[i].url;
+				var title = articles[i].title;
+				var urlToImage = articles[i].urlToImage;
+
+				html += "<div class='content-box'><div class='image'><img src="+ urlToImage +"></div><div class='headline'>"+ title +"</div></div>";
+			}
+			news.insertAdjacentHTML('beforeend', html);
 		}
 	};
 	xhr.send();
-})();
+}
+
+)();
+
+
